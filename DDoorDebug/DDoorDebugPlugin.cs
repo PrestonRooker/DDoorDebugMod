f5431c2e6b1e1dda2fb1e398b0a872b7d57d1486
Preston Rooker <prestonrooker@gmail.com>
2022-06-13 17:06:11 -0700

    Changed version number to 0.2.2
    Changed version number for new release.

--- DDoorDebug/DDoorDebugPlugin.cs
+++ DDoorDebug/DDoorDebugPlugin.cs
@@ -22,7 +22,7 @@ namespace DDoorDebug
     public class DDoorDebugPlugin : BaseUnityPlugin
     {
         const string NAME = "DDoorDebugPlugin";
-        const string VERSION = "0.2.1";
+        const string VERSION = "0.2.2";
         const string GUID = "org.bepinex.plugins.ddoordebugkz";
         //-
         public static DDoorDebugPlugin instance { get; private set; }
